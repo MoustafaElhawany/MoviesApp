6145418dfb858ee581d470c02bddafe2e8d6b856
Moustafa Elhawany <moustafa.elhawany@gmail.com>
2023-04-16 03:03:28 +0200

    feat: add ApiKey in gradle properties and set it as default in Api service

--- movies/src/main/java/com/elhawany/movies/data/remote/MoviesApiService.kt
+++ movies/src/main/java/com/elhawany/movies/data/remote/MoviesApiService.kt
@@ -1,5 +1,6 @@
 package com.elhawany.movies.data.remote
 
+import com.elhawany.core.BuildConfig
 import com.elhawany.movies.data.remote.dto.MovieDetailsResponse
 import com.elhawany.movies.data.remote.dto.MoviesListResponse
 import kotlinx.coroutines.Deferred
@@ -11,18 +12,18 @@ interface MoviesApiService {
 
     @GET("discover/movie")
     fun getMovies(
-        @Query("api_key") apiKey: String
+        @Query("api_key") apiKey: String = BuildConfig.ApiKey
     ): Deferred<MoviesListResponse>
 
     @GET("discover/movie")
     fun getMovies(
-        @Query("api_key") apiKey: String,
+        @Query("api_key") apiKey: String = BuildConfig.ApiKey,
         @Query("page") page: Int
     ): Deferred<MoviesListResponse>
 
     @GET("movie/{movie_id}")
     fun getMovieDetailsById(
         @Path("movie_id") movieId: Int,
-        @Query("api_key") apiKey: String
+        @Query("api_key") apiKey: String = BuildConfig.ApiKey
     ): Deferred<MovieDetailsResponse>
 }
