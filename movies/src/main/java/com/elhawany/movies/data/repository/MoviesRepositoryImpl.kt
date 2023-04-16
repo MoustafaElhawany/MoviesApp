02ee818ba7f7ff69dbee930aa312809fc16807a6
Moustafa Elhawany <moustafa.elhawany@gmail.com>
2023-04-16 03:16:24 +0200

    feat: remove apiKey from repository and map dto to domain models

--- movies/src/main/java/com/elhawany/movies/data/repository/MoviesRepositoryImpl.kt
+++ movies/src/main/java/com/elhawany/movies/data/repository/MoviesRepositoryImpl.kt
@@ -2,10 +2,11 @@ package com.elhawany.movies.data.repository
 
 import com.elhawany.core.utils.ResultWrapper
 import com.elhawany.core.utils.safeApiCall
-import com.elhawany.movies.data.mapper.toMovies
+import com.elhawany.movies.data.mapper.toMovie
+import com.elhawany.movies.data.mapper.toMovieDetails
 import com.elhawany.movies.data.remote.MoviesApiService
-import com.elhawany.movies.data.remote.dto.MovieDetailsResponse
 import com.elhawany.movies.domain.model.Movie
+import com.elhawany.movies.domain.model.MovieDetails
 import com.elhawany.movies.domain.repository.MoviesRepository
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
@@ -14,14 +15,14 @@ class MoviesRepositoryImpl @Inject constructor(
     private val apiService: MoviesApiService,
 ) : MoviesRepository {
 
-    override suspend fun getMoviesList(apiKey: String): ResultWrapper<List<Movie>> {
+    override suspend fun getMoviesList(): ResultWrapper<List<Movie>> {
         val response = safeApiCall(Dispatchers.IO) {
-            apiService.getMovies(apiKey)
+            apiService.getMovies()
         }
 
         return when (response) {
             is ResultWrapper.Success -> {
-                ResultWrapper.Success(response.value?.results?.toMovies())
+                ResultWrapper.Success(response.value?.results?.map { it.toMovie() })
             }
             is ResultWrapper.GenericError -> {
                 ResultWrapper.GenericError(response.code, response.error)
@@ -30,12 +31,19 @@ class MoviesRepositoryImpl @Inject constructor(
         }
     }
 
-    override suspend fun getMovieDetailsById(
-        movieId: Int,
-        apiKey: String
-    ): ResultWrapper<MovieDetailsResponse> {
-        return safeApiCall(Dispatchers.IO) {
-            apiService.getMovieDetailsById(movieId, apiKey)
+    override suspend fun getMovieDetailsById(movieId: Int): ResultWrapper<MovieDetails> {
+        val response = safeApiCall(Dispatchers.IO) {
+            apiService.getMovieDetailsById(movieId)
+        }
+
+        return when (response) {
+            is ResultWrapper.Success -> {
+                ResultWrapper.Success(response.value?.toMovieDetails())
+            }
+            is ResultWrapper.GenericError -> {
+                ResultWrapper.GenericError(response.code, response.error)
+            }
+            is ResultWrapper.NetworkError -> ResultWrapper.NetworkError
         }
     }
 
--- movies/src/main/java/com/elhawany/movies/domain/repository/MoviesRepository.kt
+++ movies/src/main/java/com/elhawany/movies/domain/repository/MoviesRepository.kt
@@ -1,11 +1,11 @@
 package com.elhawany.movies.domain.repository
 
 import com.elhawany.core.utils.ResultWrapper
-import com.elhawany.movies.data.remote.dto.MovieDetailsResponse
 import com.elhawany.movies.domain.model.Movie
+import com.elhawany.movies.domain.model.MovieDetails
 
 interface MoviesRepository {
-    suspend fun getMoviesList(apiKey: String): ResultWrapper<List<Movie>>
+    suspend fun getMoviesList(): ResultWrapper<List<Movie>>
 
-    suspend fun getMovieDetailsById(movieId: Int, apiKey: String): ResultWrapper<MovieDetailsResponse>
+    suspend fun getMovieDetailsById(movieId: Int): ResultWrapper<MovieDetails>
 }
