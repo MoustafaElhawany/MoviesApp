ed190a67ade883724dca2dcad688c7656deb95a1
Moustafa Elhawany <moustafa.elhawany@gmail.com>
2023-04-16 02:59:08 +0200

    refactor: rename ErrorMsg liveData to be UiError

--- core/src/main/java/com/elhawany/core/utils/BaseViewModel.kt
+++ core/src/main/java/com/elhawany/core/utils/BaseViewModel.kt
@@ -4,13 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.elhawany.core.R
-import com.elhawany.core.utils.ui.ErrorMsg
+import com.elhawany.core.utils.ui.UiError
 import com.elhawany.core.utils.ui.UiText
 
 open class BaseViewModel : ViewModel() {
 
-    protected val _errorMsg = MutableLiveData<ErrorMsg>()
-    val errorMsg: LiveData<ErrorMsg> = _errorMsg
+    protected val _uiError = MutableLiveData<UiError>()
+    val uiError: LiveData<UiError> = _uiError
 
     protected val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
@@ -18,13 +18,13 @@ open class BaseViewModel : ViewModel() {
     fun onResponseError(error: ResultWrapper<Any>) {
         when (error) {
             is ResultWrapper.GenericError -> {
-                _errorMsg.value = ErrorMsg(
+                _uiError.value = UiError(
                     text = UiText.StringResource(R.string.something_went_wrong)
                 )
             }
 
             is ResultWrapper.NetworkError -> {
-                _errorMsg.value = ErrorMsg(
+                _uiError.value = UiError(
                     text = UiText.StringResource(R.string.no_internet)
                 )
             }
--- core/src/main/java/com/elhawany/core/utils/ui/UiError.kt
+++ core/src/main/java/com/elhawany/core/utils/ui/UiError.kt
@@ -2,9 +2,9 @@ package com.elhawany.core.utils.ui
 
 import com.elhawany.core.utils.NotifyType
 
-data class ErrorMsg(
+data class UiError(
     val text: UiText,
-    var type: NotifyType = NotifyType.Dialog,
+    var type: NotifyType = NotifyType.SnackBar,
     var cancelable: Boolean = true,
 )
 
